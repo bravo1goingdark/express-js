6c23764fe70a0b0a7129e9f41bd7efdfa73f60fd
bravo1goingdark <kumarashutosh34169@gmail.com>
2024-05-29 13:36:05 +0530

    signed and unsigned cookies

--- src/cookies-05.mjs
+++ src/cookies-05.mjs
@@ -5,10 +5,11 @@ const app = express();
 
 app.use(cookieParser("secret"));
 app.get('/cookies' , (request,response) => {
-    response.cookie("hello" , "world" , {maxAge: 10000 * 60 , signed:true});
+    response.cookie("hello" , "world" , {maxAge: 10000 , signed:true});
     console.log(request.headers.cookie); // log name-value pair before pasrsing using cookie-parser
     console.log(request.cookies); // logs after using cookie-parser
-    if (request.cookies.hello && request.cookies.hello === "world") {
+    // for unsigned cookies we can use -> request.cookies.hello && request.cookies.hello === "world"
+    if (request.signedCookies.hello && request.signedCookies.hello === "world") {
         return response.send("send you a cookie");
     }
     return response.status(403).send({msg: "sorry no cookies"});
@@ -16,4 +17,4 @@ app.get('/cookies' , (request,response) => {
 
 app.listen(3000 , () => {
     console.log(`server running on ${3000}`);
-})
+});
